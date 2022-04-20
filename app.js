f92d36ae6351051d89c22ab5ad9c1a498afb55b2
Dmitrij <dmitry.bermas@gmail.com>
2022-04-20 10:55:55 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+function game() {
+    const startBtn = document.querySelector('#start')
+    const screens = document.querySelectorAll('.screen')
+    const timeList = document.querySelector('#time-list')
+    const timeEl = document.querySelector('#time')
+    const board = document.querySelector('#board')
+    const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+    const againBtn = document.querySelector('#again')
+    let time = 0
+    let score = 0 
+
+    startBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        screens[0].classList.add('up')
+    })
+    
+    timeList.addEventListener('click', event => {
+        if (event.target.classList.contains('time-btn')) {
+            time = parseInt(event.target.getAttribute('data-time'))
+            screens[1].classList.add('up')
+            startGame()
+        }
+    })
+    
+    board.addEventListener('click' , event => {
+        if(event.target.classList.contains('circle')) {
+            score++
+            event.target.remove() 
+            createRandomCircle()
+        }
+    })
+    
+    function startGame() {
+        setInterval(decreaseTime,1000)
+        createRandomCircle()
+        timeEl.innerHTML = `00:${time}`
+        setTime(time)
+    }
+    
+    function decreaseTime() {
+        if (time === 0) {
+            finishGame()
+        } else {
+            let current = --time
+            if (current < 10) {
+                current = `0${current}`
+            }
+            timeEl.innerHTML = `00:${current}`
+            setTime(current)
+        }
+    }
+    
+    function setTime(value) {
+        timeEl.innerHTML = `00:${value}`
+    }
+    
+    function finishGame() {
+        timeEl.parentNode.classList.add('hide')
+        board.innerHTML = `<h1>Ваш счёт: <span class="primary">${score}</span></h1>`
+    }
+    
+    function createRandomCircle() {
+        const circle = document.createElement('div')
+        const size = getRandomNumber(10 , 60)
+        const {width, height} = board.getBoundingClientRect()
+        const x = getRandomNumber(0, width - size)
+        const y = getRandomNumber(0, height - size)
+        circle.classList.add('circle')
+        setColor(circle)
+        circle.style.width = `${size}px`
+        circle.style.height = `${size}px`
+        circle.style.top = `${y}px`
+        circle.style.left = `${x}px`
+        board.append(circle)
+    }
+    
+    function getRandomNumber(min, max) {
+        return Math.round(Math.random()*(max - min) + min)
+    }
+    
+    function setColor(element) {
+        const color = getRandomColor()
+        element.style.background = color
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    }
+    
+    function getRandomColor() {
+        const index = Math.floor(Math.random() * colors.length)
+        return colors[index]
+    }
+}
+
+game()
